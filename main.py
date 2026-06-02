620dd4d281f765cd7eccfe3df34c721668109be5
Peter Kiprop <PKiprop@Caldwell.edu>
2026-06-02 08:29:53 +0000

    ${2AM4:29 AMe4:29 AMi29e}

--- main.py
+++ main.py
@@ -113,3 +113,7 @@ def all_completions() -> dict[str, int]:
 def level(n: int) -> int:
     if n == 0: return 0
     if n <= 2: return 1
+    if n <= 5: return 2
+    if n <= 9: return 3
+    return 4
+
